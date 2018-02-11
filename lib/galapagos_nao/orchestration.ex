be8b067c33b3dc9d85d078e530345ca6e6179fee
Jeff Smith <jeffreyksmithjr@gmail.com>
2018-02-11 23:20:41 +0800

    multi-generation evolution

--- lib/galapagos_nao/orchestration.ex
+++ lib/galapagos_nao/orchestration.ex
@@ -2,6 +2,7 @@ defmodule GN.Orchestration do
   import GN.Gluon
   import GN.Evolution, only: [spawn_offspring: 1, build_layer: 2]
   alias GN.Network, as: Network
+  import GN.Selection, only: [select: 1]
 
   # 5 minutes
   @timeout 300_000
@@ -24,19 +25,40 @@ defmodule GN.Orchestration do
     %Network{layers: layers, test_acc: test_acc}
   end
 
+  def learn_generation(%Network{} = initial_net) do
+    learn_generation(%{-1 => initial_net})
+  end
+
   def learn_generation(nets) do
     batch_size = (@max_parallel / map_size(nets)) |> Statistics.Math.to_int()
 
-    for {_level, net} <- nets do
-      seed_layers = net.layers
-      pmap(1..batch_size, fn _n -> start_and_spawn(seed_layers) end)
-    end
-    |> Enum.flat_map(fn n -> n end)
+    generation =
+      for {_level, net} <- nets do
+        seed_layers = net.layers
+        pmap(1..batch_size, fn _n -> start_and_spawn(seed_layers) end)
+      end
+      |> Enum.flat_map(fn n -> n end)
+
+    inspect_generation(generation)
+
+    generation
   end
 
   def inspect_generation(nets) do
     for net <- nets do
       IO.puts(inspect(net))
     end
+  end
+
+  def evolve(net, generations) when generations > 0 do
+    IO.puts("Generations remaining: #{generations}")
+
+    learn_generation(net)
+    |> select()
+    |> evolve(generations - 1)
+  end
+
+  def evolve(net, _generations) do
+    net
   end
 end
--- test/galpagos_nao/orchestration_test.exs
+++ test/galpagos_nao/orchestration_test.exs
@@ -4,7 +4,7 @@ defmodule GN.OrchestrationTest do
 
   test "parallel map is equivalent to map" do
     range = 1..10
-    function = &(&1*&1)
+    function = &(&1 * &1)
     expectation = Enum.map(range, function) |> Enum.into(MapSet.new())
     returned = pmap(range, function) |> Enum.into(MapSet.new())
     assert returned == expectation
